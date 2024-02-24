555898f961cff06ca5a18ef1476eb640d2f23e74
CryingBaker <atharvinfo1811@gmail.com>
2024-02-24 15:18:21 +0530

    Removed unneccessary files

--- js/script.js
+++ js/script.js
@@ -2,141 +2,141 @@ let toggleBtn = document.getElementById('toggle-btn');
 let body = document.body;
 let darkMode = localStorage.getItem('dark-mode');
 
-const enableDarkMode = () =>{
-   toggleBtn.classList.replace('fa-sun', 'fa-moon');
-   body.classList.add('dark');
-   localStorage.setItem('dark-mode', 'enabled');
+const enableDarkMode = () => {
+    toggleBtn.classList.replace('fa-sun', 'fa-moon');
+    body.classList.add('dark');
+    localStorage.setItem('dark-mode', 'enabled');
 }
 
-const disableDarkMode = () =>{
-   toggleBtn.classList.replace('fa-moon', 'fa-sun');
-   body.classList.remove('dark');
-   localStorage.setItem('dark-mode', 'disabled');
+const disableDarkMode = () => {
+    toggleBtn.classList.replace('fa-moon', 'fa-sun');
+    body.classList.remove('dark');
+    localStorage.setItem('dark-mode', 'disabled');
 }
 
-if(darkMode === 'enabled'){
-   enableDarkMode();
+if (darkMode === 'enabled') {
+    enableDarkMode();
 }
 
-toggleBtn.onclick = (e) =>{
-   darkMode = localStorage.getItem('dark-mode');
-   if(darkMode === 'disabled'){
-      enableDarkMode();
-   }else{
-      disableDarkMode();
-   }
+toggleBtn.onclick = (e) => {
+    darkMode = localStorage.getItem('dark-mode');
+    if (darkMode === 'disabled') {
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
 }
 
 let profile = document.querySelector('.header .flex .profile');
 
-document.querySelector('#user-btn').onclick = () =>{
-   profile.classList.toggle('active');
-   search.classList.remove('active');
+document.querySelector('#user-btn').onclick = () => {
+    profile.classList.toggle('active');
+    search.classList.remove('active');
 }
 
 let search = document.querySelector('.header .flex .search-form');
 
-document.querySelector('#search-btn').onclick = () =>{
-   search.classList.toggle('active');
-   profile.classList.remove('active');
+document.querySelector('#search-btn').onclick = () => {
+    search.classList.toggle('active');
+    profile.classList.remove('active');
 }
 
 let sideBar = document.querySelector('.side-bar');
 
-document.querySelector('#menu-btn').onclick = () =>{
-   sideBar.classList.toggle('active');
-   body.classList.toggle('active');
+document.querySelector('#menu-btn').onclick = () => {
+    sideBar.classList.toggle('active');
+    body.classList.toggle('active');
 }
 
-document.querySelector('#close-btn').onclick = () =>{
-   sideBar.classList.remove('active');
-   body.classList.remove('active');
+document.querySelector('#close-btn').onclick = () => {
+    sideBar.classList.remove('active');
+    body.classList.remove('active');
 }
 
-window.onscroll = () =>{
-   profile.classList.remove('active');
-   search.classList.remove('active');
+window.onscroll = () => {
+    profile.classList.remove('active');
+    search.classList.remove('active');
 
-   if(window.innerWidth < 1200){
-      sideBar.classList.remove('active');
-      body.classList.remove('active');
-   }
+    if (window.innerWidth < 1200) {
+        sideBar.classList.remove('active');
+        body.classList.remove('active');
+    }
 }
 particlesJS("background", {
 
-   particles: {
-       number: {
-           value: 15, // Number of Particles (count)
-           density: {
-               enable: true,
-               value_area: 300, // Area where particles will be distributed
-           },
-       },
+    particles: {
+        number: {
+            value: 15, // Number of Particles (count)
+            density: {
+                enable: true,
+                value_area: 300, // Area where particles will be distributed
+            },
+        },
 
-       color: {
-           value: "#2196f3", // Particles color
-       },
-       shape: {
-           type: "polygon", // Shape type
-       },
-       opacity: {
-           value: 0.9, // Base opacity of particles
-           random: true,
-           anum: {
-               enable: true,
-               speed: 1,
-               opacity_min: 0.1,
-               sync: false,
-           },
-       },
-       size: {
-           value: 5, // Base size of particles
-           random: true,
-           anim: {
-               enable: true,
-               speed: 4,
-               size_min: 0.3,
-               sync: false,
-           },
-       },
+        color: {
+            value: "#2196f3", // Particles color
+        },
+        shape: {
+            type: "polygon", // Shape type
+        },
+        opacity: {
+            value: 0.9, // Base opacity of particles
+            random: true,
+            anum: {
+                enable: true,
+                speed: 1,
+                opacity_min: 0.1,
+                sync: false,
+            },
+        },
+        size: {
+            value: 5, // Base size of particles
+            random: true,
+            anim: {
+                enable: true,
+                speed: 4,
+                size_min: 0.3,
+                sync: false,
+            },
+        },
 
-       // Connecting lines
-       line_linked: {
-           enable: true,
-           distance: 150, // Maximum distance between linked particles
-           color: "e91e63",
-           opacity: 0.4,
-           width: 1,
-       },
+        // Connecting lines
+        line_linked: {
+            enable: true,
+            distance: 150, // Maximum distance between linked particles
+            color: "e91e63",
+            opacity: 0.4,
+            width: 1,
+        },
 
-       // Particle movement
-       move: {
-           enable: true,
-           speed: 2,
-           direction: "none",
-           random: false,
-           straight: false,
-           out_mode: "bounce", // Behavior when particles move out of the canvas
-           bounce: false,
-       },
-   },
-   // Interactivity settings
-   interactivity: {
-       detect_on: "canvas",
-       events: {
-           onhover: {
-               enable: true, // Enable hover interactivity
-               mode: "repulse",
-           },
-           onclick: {
-               enable: true, // Enable for click
-               mode: "push", // Push particles on click
-           },
-           resize: true, // Resize particles animation on window resize
-       },
-   },
+        // Particle movement
+        move: {
+            enable: true,
+            speed: 2,
+            direction: "none",
+            random: false,
+            straight: false,
+            out_mode: "bounce", // Behavior when particles move out of the canvas
+            bounce: false,
+        },
+    },
+    // Interactivity settings
+    interactivity: {
+        detect_on: "canvas",
+        events: {
+            onhover: {
+                enable: true, // Enable hover interactivity
+                mode: "repulse",
+            },
+            onclick: {
+                enable: true, // Enable for click
+                mode: "push", // Push particles on click
+            },
+            resize: true, // Resize particles animation on window resize
+        },
+    },
 
-   // Detect retina displays
-   retina_detect: true,
+    // Detect retina displays
+    retina_detect: true,
 
 });
